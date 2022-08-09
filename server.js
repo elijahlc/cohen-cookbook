23dbe2e180c93dc98ed16f0ae6ceb47dfc90845c
Eli Cohen <cohen.elijahlev@gmail.com>
2022-08-09 17:48:30 -0400

    updated port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 const setup = () => {
-	const port = `${process.env.PORT}?sslmode=no-verify` || 3000;
+	const port = process.env.PORT || 3000;
 
 	app.listen(port, () => {
 		console.log(`app listening on port ${port}`);
