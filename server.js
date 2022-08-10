9fa2714dcb34e0ff0141608fd91627a68b554e45
Eli Cohen <cohen.elijahlev@gmail.com>
2022-08-10 07:23:52 -0400

    added error handling and updated mobile responsiveness

--- server.js
+++ server.js
@@ -16,6 +16,43 @@ app.get('/', (req, res) => {
 	res.redirect('/recipes');
 });
 
+app.use((req, res, next) => {
+	const err = new Error('Not found');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	res.status(err.status || 500).send(`
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<title>Cohen Cookbook</title>
+			<link rel="stylesheet" href="../styles.css" />
+		</head>
+		<body>
+			<header>
+				<h1><a href="/recipes">Our Cookbook</a></h1>
+				<nav>
+					<a href="/recipes">Recipes</a>
+					<a href="/ingredients">Ingredients</a>
+					<a href="/tags">Tags</a>
+				</nav>
+			</header>
+			<main class=error>
+				<h2>Error! Tell Eli to fix something.</h2>
+				<p>Error is: ${err} at ${req.path}</p>
+			</main class=error>
+			<footer>
+				<div>made by <a href='https://github.com/elijahlc'>elijah</a> // august 2022</div>
+			</footer>
+		</body>
+	</html>
+	`);
+});
+
 const setup = () => {
 	const port = process.env.PORT || 3000;
 
