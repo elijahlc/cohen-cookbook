238796f0c63fe38638ab589472d7a0c6000e8877
Eli Cohen <cohen.elijahlev@gmail.com>
2022-08-09 20:23:29 -0400

    config ssl

--- db/index.js
+++ db/index.js
@@ -1,8 +1,12 @@
 const pg = require('pg');
 
-const client = new pg.Client(
-	process.env.DATABASE_URL || 'postgres://localhost/cohen_cookbook'
-);
+const client = new pg.Client({
+	connectionString:
+		process.env.DATABASE_URL || 'postgres://localhost/cohen_cookbook',
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
 
 client.connect();
 
